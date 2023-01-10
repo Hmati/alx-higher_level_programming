3bdcc6512d9458bd1923d04cea0d025f5bfc626b
Hmati <mosesmati93@gmail.com>
2023-01-10 07:55:16 -0800

    0x0B. Python - Input/Output

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+""" json representation of an object(string)"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+
+    """returns the json representation of an object"""
+
+    data = json.dumps(my_obj)
+
+    return data
---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+"""Returns python data structure"""
+
+
+import json
+
+
+def from_json_string(my_str):
+
+    """returns an object(Python data structure)
+
+    represented by a json string"""
+
+    data = json.loads(my_str)
+
+    return data
